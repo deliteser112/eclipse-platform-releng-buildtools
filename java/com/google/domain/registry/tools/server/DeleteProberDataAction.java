1c7c0709836420defce67fd64a2db2df8a1f4860
deliteser112 <deliteser112@gmail.com>
2016-04-01 13:33:46 -0700

    Merge tools/[] package into tools/server
    There's no point in having this extra package exclusively for
    []s, especially because we already weren't being consistent and
    had multiple []s in the tools/server package.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=118807843

--- java/com/google/domain/registry/tools/server/DeleteProberDataAction.java
+++ java/com/google/domain/registry/tools/server/DeleteProberDataAction.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.domain.registry.tools.mapreduce;
+package com.google.domain.registry.tools.server;
 
 import static com.google.common.base.Verify.verifyNotNull;
 import static com.google.domain.registry.mapreduce.MapreduceRunner.PARAM_DRY_RUN;
