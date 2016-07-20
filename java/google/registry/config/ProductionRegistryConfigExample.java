e71f497c330da98822eb6b8c6f4dfb3ef15f3849
deliteser112 <deliteser112@gmail.com>
2016-07-20 14:40:24 -0400

    Clarify the relationship between project ids in RegistryConfigs
    This makes it clear that the non-production environment project ids are all
    constructed by adding a suffix to the production environment project id, and
    makes it slightly easier to configure it for a new environment (because there is
    only one string to change instead of two).
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=135406763

--- java/google/registry/config/ProductionRegistryConfigExample.java
+++ java/google/registry/config/ProductionRegistryConfigExample.java
@@ -53,11 +53,12 @@ public final class ProductionRegistryConfigExample implements RegistryConfig {
 
   @Override
   public String getProjectId() {
+    String prodProjectId = "domain-registry";
     switch (environment) {
       case PRODUCTION:
-        return "domain-registry";
+        return prodProjectId;
       default:
-        return "domain-registry-" + Ascii.toLowerCase(environment.name());
+        return prodProjectId + "-" + Ascii.toLowerCase(environment.name());
     }
   }
 
