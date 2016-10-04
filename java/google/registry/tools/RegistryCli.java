24341c238f49c0b265f03d34e10582f3e692f633
deliteser112 <deliteser112@gmail.com>
2016-10-04 06:58:23 -0700

    Remove last stray mention of gTech
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=135101061

--- java/google/registry/tools/RegistryCli.java
+++ java/google/registry/tools/RegistryCli.java
@@ -50,7 +50,7 @@ final class RegistryCli {
 
   // The <? extends Class<? extends Command>> wildcard looks a little funny, but is needed so that
   // we can accept maps with value types that are subtypes of Class<? extends Command> rather than
-  // literally that type (e.g. Class<? extends GtechCommand>).  For more explanation, see:
+  // literally that type.  For more explanation, see:
   //   http://www.angelikalanger.com/GenericsFAQ/FAQSections/TypeArguments.html#FAQ104
   void run(
       String programName,
