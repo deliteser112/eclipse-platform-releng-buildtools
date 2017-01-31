c5ea3b7ffba62c79224c36c3603bc9c6ef6a0ce4
deliteser112 <deliteser112@gmail.com>
2017-01-31 06:08:36 -0800

    Update copyright year on all license headers
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=146111211

--- scripts/setup_eclipse.py
+++ scripts/setup_eclipse.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# Copyright 2016 The Nomulus Authors. All Rights Reserved.
+# Copyright 2017 The Nomulus Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 
 """Script for generating eclipse .project and .classpath files."""
 
