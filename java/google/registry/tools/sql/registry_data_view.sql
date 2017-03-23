bb7f3f08ffb38631d8094f7377b91bf85ce5a14f
deliteser112 <deliteser112@gmail.com>
2017-03-23 10:22:01 -0700

    Remove now-unused RegistryData SQL columns
    These columns were only necessary to support the old PremiumListData and
    RecurringEventData views, which were removed in []
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=151019824

--- java/google/registry/tools/sql/registry_data_view.sql
+++ java/google/registry/tools/sql/registry_data_view.sql
@@ -21,22 +21,6 @@
 SELECT
   tldStr AS tld,
   tldType AS type,
-  -- This relies on the fact that we currently haven't ever used more than one
-  -- value with renewBillingCostTransitions.
-  -- TODO(b/20764952): fix this limitation.
-  FIRST(renewBillingCostTransitions.billingCost) WITHIN RECORD
-    AS renewBillingCost,
-  -- Grace period lengths are stored in the ISO 8601 duration format as a
-  -- duration in seconds, which produces a string of the form "PT####S".
-  INTEGER(REGEXP_EXTRACT(autoRenewGracePeriodLength, r'PT(.+)S'))
-    AS autorenewGracePeriodSeconds,
-  premiumList.path AS premiumListPath,
-  -- TODO(b/18265521): remove this column once the referenced bug is fixed;
-  -- this column is only included because without it BigQuery will complain
-  -- that we aren't consuming reservedLists (due to it having a repeated .path
-  -- child field, which overlaps with premiumList.path above).
-  GROUP_CONCAT_UNQUOTED(reservedLists.path, "/") WITHIN RECORD
-    AS reservedListPaths,
 FROM
   [%SOURCE_DATASET%.Registry]
 ORDER BY
