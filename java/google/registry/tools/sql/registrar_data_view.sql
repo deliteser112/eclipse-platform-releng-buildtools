2899116d19310adb261c7174dc79aaf7a90248bf
deliteser112 <deliteser112@gmail.com>
2017-05-10 15:17:19 -0700

    Add type to RegistrarData billing BigQuery view
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=155682529

--- java/google/registry/tools/sql/registrar_data_view.sql
+++ java/google/registry/tools/sql/registrar_data_view.sql
@@ -14,14 +14,17 @@
 
 -- Registrar Data View SQL
 --
--- This query lists registrar IDs with billing IDs and allowed TLDs.
+-- This query lists each registrar ID with type, billing ID, and allowed TLDs.
+--
+-- Note that there is one row per registrar (allowedTlds is repeated), versus
+-- registrar_account_data_view.sql which has multiple rows per registrar.
 SELECT
   __key__.name AS registrarId,
+  type,
   billingIdentifier AS billingId,
   allowedTlds
 FROM
   [%SOURCE_DATASET%.Registrar]
--- TODO(b/19031620): Add filter to just include registrars with type=REAL.
 -- Note: We can't ORDER BY registrarId here because ORDER/GROUP BY will
 --   flatten results, and the allowedTlds field above is repeated.
 -- TODO(b/19031339): Add "ORDER BY" if the BigQuery known issue is fixed.
