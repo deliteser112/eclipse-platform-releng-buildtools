d9d2c89fb0b2cb53cac04b17e85f1545f7a65909
deliteser112 <deliteser112@gmail.com>
2019-02-12 09:13:04 -0800

    Set up the plugin build environment
    The goal of this CL is to set up the build environment to allow plugins to work.
    
    We have a trivial plugin that doesn't do anything (yet) - it just sets itself as the finalizer of all Reporting tasks.
    
    Eventually, this plugin will upload all reports to GCS, and even create a "cover page" linking to each one of them.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=233617499

---
+++ gradle/buildSrc/src/main/java/google/registry/gradle/plugin/GcsReportUploaderPlugin.java
@@ -0,0 +1,43 @@
+// Copyright 2019 The Nomulus Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package google.registry.gradle.plugin;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+/**
+ * Plugin setting up the GcsReportUploader task.
+ *
+ * <p>It goes over all the tasks in a project and pass them on to the GcsReportUploader task for set
+ * up.
+ *
+ * <p>Note that since we're passing in all the projects' tasks - this includes the GcsReportUploader
+ * itself! It's up to the GcsReportuploader to take care of not having "infinite loops" caused by
+ * waiting for itself to end before finishing.
+ */
+public class GcsReportUploaderPlugin implements Plugin<Project> {
+
+  public void apply(Project project) {
+    GcsReportUploader reportUploader =
+        project.getTasks().create("gcsReportUploader", GcsReportUploader.class, task -> {
+          task.setDescription("Uploads the reports to GCS bucket");
+          task.setGroup("uploads");
+        });
+
+    for (Project subProject : project.getAllprojects()) {
+      subProject.getTasks().all(reportUploader::addTask);
+    }
+  }
+}
