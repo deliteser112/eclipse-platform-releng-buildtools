124724aed3595d2939f4f32ba6b728daad9d2a1d
deliteser112 <deliteser112@gmail.com>
2019-09-27 15:12:59 -0400

    Add sql scripts to create other types of user (#283)
    1. User with read-only permission to all tables
    2. User with read-write permission to schema and all tables

---
+++ db/src/main/resources/sql/user/create_readonly_user.sql
@@ -0,0 +1,21 @@
+-- Copyright 2019 The Nomulus Authors. All Rights Reserved.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+-- Script to create a user with read-only permission to all tables.
+
+CREATE USER :username ENCRYPTED PASSWORD :'password';
+GRANT CONNECT ON DATABASE postgres TO :username;
+GRANT USAGE ON SCHEMA public TO :username;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO :username;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO :username;
