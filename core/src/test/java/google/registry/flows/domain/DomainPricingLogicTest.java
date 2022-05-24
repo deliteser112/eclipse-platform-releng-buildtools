7f2c22476fa74290d0cf85ce126efe9219038baf
deliteser112 <deliteser112@gmail.com>
2022-05-24 17:22:08 -0400

    Fix style warning (#1643)
    THe build is giving a style warning over the extra parens.

--- core/src/test/java/google/registry/flows/domain/DomainPricingLogicTest.java
+++ core/src/test/java/google/registry/flows/domain/DomainPricingLogicTest.java
@@ -117,7 +117,7 @@ public class DomainPricingLogicTest {
             new BillingEvent.Recurring.Builder()
                 .setParent(historyEntry)
                 .setRegistrarId(domain.getCreationRegistrarId())
-                .setEventTime((DateTime.parse("1999-01-05T00:00:00Z")))
+                .setEventTime(DateTime.parse("1999-01-05T00:00:00Z"))
                 .setFlags(ImmutableSet.of(AUTO_RENEW))
                 .setId(2L)
                 .setReason(Reason.RENEW)
