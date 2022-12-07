b95a46ea18efa3f73e9fe05b64dfc9d083f5d34f
deliteser112 <deliteser112@gmail.com>
2022-12-07 12:35:29 -0500

    Replace appengine repackaged class with the proper class (#1871)

--- core/src/main/java/google/registry/tools/GetReservedListCommand.java
+++ core/src/main/java/google/registry/tools/GetReservedListCommand.java
@@ -16,7 +16,7 @@ package google.registry.tools;
 
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
-import com.google.appengine.repackaged.com.google.common.collect.Streams;
+import com.google.common.collect.Streams;
 import google.registry.model.tld.label.ReservedList.ReservedListEntry;
 import google.registry.model.tld.label.ReservedListDao;
 import java.util.Comparator;
